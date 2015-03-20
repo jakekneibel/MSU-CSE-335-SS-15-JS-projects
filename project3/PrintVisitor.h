2510bfa9f8a2b5c1e2ffddb2185b331179d46395
sschmitz705 <sschmitz705@aim.com>
2015-03-20 14:19:16 -0400

    Create PrintVisitor.h

---
+++ project3/PrintVisitor.h
@@ -0,0 +1,11 @@
+#ifndef PRINTVISITOR_H
+#define	PRINTVISITOR_H
+class PrintVisitor{
+public
+PrintVisitor();
+PrintVisitor(const PrintVisitor&);
+~PrintVisitor();
+PrintVisitor& operator=(const PrintVisitor&);
+};
+
+#endif	/* PRINTVISITOR_H */
