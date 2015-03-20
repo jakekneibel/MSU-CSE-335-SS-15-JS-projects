f55bfc4e2a69d4938894b94c157c705e13b6bdc9
sschmitz705 <sschmitz705@aim.com>
2015-03-20 14:13:44 -0400

    Create SearchVisitor.h

---
+++ project3/SearchVisitor.h
@@ -0,0 +1,12 @@
+#ifndef SEARCHVISITOR_H
+#define	SEARCHVISITOR_H
+class SearchVisitor{
+public
+  SearchVisitor();
+  SearchVisitor(const SearchVisitor&);
+  ~SearchVisitor();
+  SearchVisitor& operator=(const SearchVisitor&);
+};
+
+
+#endif	/* SEARCHVISITOR_H */
