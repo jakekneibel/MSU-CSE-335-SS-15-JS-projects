e528771de3b1ddf338f03cccd0a65516d0c7c361
sschmitz705 <sschmitz705@aim.com>
2015-03-22 14:13:44 -0400

    Update Group.h

--- project3/Group.h
+++ project3/Group.h
@@ -1,10 +1,26 @@
 #ifndef GROUP_H
 #define	GROUP_H
-class Group{
+#include <list>
+class Groups{
 public:
+  virtual void Accept(Visitor*v)=0;
+};
+class Group: public Groups{
+private:
+  string name;
+  Groups list;
+  
+public:
+  virtual void Accept(Visitor*v){v->VisitGroup(this);}
   Group();
   Group(const Group&);
   ~Group();
   Group& operator=(const Group&);
+  string getName(){return name;}
+  groups list(){return list;}
+  void AddChild(Groups*);
+  int GetChildenSize(){return list.size();}
+  Groups GetChild(int i){return list[i];}
+  
 };
 #endif	/* GROUP_H */
