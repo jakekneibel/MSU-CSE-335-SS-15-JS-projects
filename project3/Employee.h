1259dc6c522402072b50706f19088d68ea6bed25
sschmitz705 <sschmitz705@aim.com>
2015-03-22 14:29:27 -0400

    Update Employee.h

--- project3/Employee.h
+++ project3/Employee.h
@@ -1,12 +1,21 @@
 #ifndef EMPLOYEE_H
 #define	EMPLOYEE_H
 
-class Employee{
+class Employee :public Node{
+private:
+  string fName;
+  string lName;
+  string pos;
 public:
+  virtual void Accept(Visitor*v){v->VisitEmployee(this);}
   Employee();
+  Employee(string theFName,string theLName,string thepos);
   Employee(const Employee&);
   ~Employee();
   Employee& operator=(const Employee&);
+  string FName(){return fName;}
+  string LName(){return lName;}
+  string pos(){return pos;}
 };
 
 
