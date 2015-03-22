447da7b3afec9aeb60c61109a60d4786f0f20735
sschmitz705 <sschmitz705@aim.com>
2015-03-22 14:29:30 -0400

    Update Employee.cpp

--- project3/Employee.cpp
+++ project3/Employee.cpp
@@ -1,6 +1,12 @@
 #include "Employee.h"
-Employee::Employee(){
+Employee::Employee(string theFName,string theLName,string thepos){
+  fName=theFName;
+  lName=theLName;
+  pos=thepos;
 }
 Employee::Employee(const Employee & employee){
+  fName=employee.FName();
+  lName=employee.LName();
+  pos=employee.pos();
 }
 Employee::~Employee(){}
