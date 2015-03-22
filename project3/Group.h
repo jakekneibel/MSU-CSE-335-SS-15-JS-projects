1ffbcfbdc1825a2e612e1f8f954f238ddda5d82a
sschmitz705 <sschmitz705@aim.com>
2015-03-22 14:31:23 -0400

    Update Group.h

--- project3/Group.h
+++ project3/Group.h
@@ -17,11 +17,11 @@ public:
   Group(const Group&);
   ~Group();
   Group& operator=(const Group&);
-  string getName(){return name;}
-  groups list(){return list;}
+  string getName(){return name;};
+  groups list(){return list;};
   void AddChild(Node*);
-  int GetChildenSize(){return list.size();}
-  Node GetChild(int i){return list[i];}
+  int GetChildenSize(){return list.size();};
+  Node GetChild(int i){return list[i];};
   
 };
 #endif	/* GROUP_H */
