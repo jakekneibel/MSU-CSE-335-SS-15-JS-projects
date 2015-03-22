aa962d672cc30545167f210a9b3518d032192047
sschmitz705 <sschmitz705@aim.com>
2015-03-22 14:13:46 -0400

    Update Group.cpp

--- project3/Group.cpp
+++ project3/Group.cpp
@@ -1,6 +1,12 @@
 #include "Group.h"
-Group::Group(){
+Group::Group(string theName){
+  name=theName;
 }
 Group::Group(const Group & group){
+  name=group.getName();
+  list=group.list();
 }
 Group::~Group(){}
+Group::AddChild(Groups* x){
+  list.push_back(x);
+}
