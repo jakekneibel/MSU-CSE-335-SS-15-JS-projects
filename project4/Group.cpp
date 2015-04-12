aa90f55dea4209bd309031957111042cbcf681a0
sschmitz705 <sschmitz705@aim.com>
2015-04-12 17:49:12 -0400

    Update Group.cpp

--- project4/Group.cpp
+++ project4/Group.cpp
@@ -18,38 +18,53 @@ Group::deletes(){
 }
 void Group::deletion(std::string node){
   for(int x=0; x<nlist.GetChildrenSize(); x++){
-    if(x.getName()==nodes){
-      x.deletes();
+    if(nlist[x].getName()==nodes){
+      nlist[x].deletes();
     }
-    else if(x.type==2){
-      Node* ref=root[x];
+    else if(nlist[x].type==2){
+      Node* ref=nlist[x];
       ref.deletion(nodes);
     }
   }
 }
 void Group::deletionEmployee(fname, lname, pos){
   for(int x=0; x<nlist.GetChildrenSize(); x++){
-    if(x.getFirst()==fname && x.GetLast()==lname && x.getField()==pos){
-      x.deletes();
+    if(nlist[x].type==2){
+      if(nlist[x].getFirst()==fname && nlist[x].GetLast()==lname && nlist[x].getField()==pos){
+        nlist[x].deletes();
+      }
     }
-    else if(x.type==2){
-      Node* ref=root[x];
+    else if(nlist[x].type==2){
+      Node* ref=nlist[x];
       ref.deletionEmployee(fname, lname, pos);
     }
   }
-};
+}
 void Group::disband(nodes){
   for(int x=0; x<nlist.GetChildrenSize(); x++){
-    if(x.getName()==nodes){
-      Node* ref=root[x];
+    if(nlist[x].getName()==nodes){
+      Node* ref=nlist[x];
       for(int i=0; i<ref.GetChildrenSize(); i++){
         AddChild(ref.GetChild(i));
       }
-      delete x;
+      delete nlist[x];
     }
-    else if(x.type==2){
-      Node* ref=root[x];
+    else if(nlist[x].type==2){
+      Node* ref=nlist[x];
       ref.disbands(nodes);
+    }
+  }
+}
+void Group::addnotes(nodes, group){
+  for(int x=0; x<nlist.GetChildrenSize(); x++){
+    if(nlist[x].type==2){
+      Node* ref=nlist[x];
+      if(ref.getName()==group){
+        ref.AddChild(nodes);
+      }
+      else{
+        ref.addnotes(nodes, group);
+      }
     }
   }
 }
