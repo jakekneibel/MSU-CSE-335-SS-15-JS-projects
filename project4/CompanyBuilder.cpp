1b463476d895a555c7a925efd446f17e7ce3f41b
sschmitz705 <sschmitz705@aim.com>
2015-04-12 17:40:14 -0400

    Update CompanyBuilder.cpp

--- project4/CompanyBuilder.cpp
+++ project4/CompanyBuilder.cpp
@@ -1,5 +1,23 @@
+
 #include "CompanyBuilder.h"
+void CompanyBuilder::BuildCompany(){Group* root = new Group(" ");};
+void CompanyBuilder::addNode(nodes, group){
+  root.addnotes(nodes, group);
+}
+void CompanyBuilder::addroot(roots){Group* root = new Group(roots);};
 CompanyBuilder::CompanyBuilder(const CompanyBuilder & company){
   root=company.getroot();
 }
+void CompanyBuilder::disband(nodes){
+  root.disbands(nodes);
+}
+void CompanyBuilder::deleteGroup(nodes){
+  root.deletion(nodes);
+}
+virtual void CompanyBuilder::deleteEmployee(fname, lname, pos){
+  root.deletionEmployee(fname, lname, pos);
+}
 CompanyBuilder::~CompanyBuilder(){}
+CompanyBuilder::CompanyBuilder(const Group & group){
+  root=group.getroot();
+}
