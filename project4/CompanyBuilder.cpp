360606a623aaee8aa9a0c37d05dd4daae7d760f0
sschmitz705 <sschmitz705@aim.com>
2015-04-12 18:01:06 -0400

    Update CompanyBuilder.cpp

--- project4/CompanyBuilder.cpp
+++ project4/CompanyBuilder.cpp
@@ -2,7 +2,14 @@
 #include "CompanyBuilder.h"
 void CompanyBuilder::BuildCompany(){Group* root = new Group(" ");};
 void CompanyBuilder::addNode(nodes, group){
-  root.addnotes(nodes, group);
+  int flag;
+  flag=root.addnotes(nodes, group,0);
+  if(flag==0 && nodes.type==2){
+    cout<<"ERROR: parent group "<<node.getName()<<" does not exist. Omitting this entry"<<endl;
+  }
+  else if(flag==0 && nodes.type==1){
+    cout<<"ERROR: parent group "<<node.getFirst()<<" does not exist. Omitting this entry"<<endl;
+  }
 }
 void CompanyBuilder::addroot(roots){Group* root = new Group(roots);};
 CompanyBuilder::CompanyBuilder(const CompanyBuilder & company){
