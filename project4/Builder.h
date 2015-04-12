5516de6914bce3cc09bad72151c698195359b6a2
sschmitz705 <sschmitz705@aim.com>
2015-04-12 18:08:33 -0400

    Create Builder.h

---
+++ project4/Builder.h
@@ -0,0 +1,12 @@
+#ifndef BUILDER_H
+#define	BUILDER_H
+class Builder {
+public:
+  virtual void BuildCompany()=0;
+  virtual void addNode(Node* nodes, std::string group)=0;
+  virtual void addroot(std::string roots)=0;
+  virtual void disband(std::string nodes)=0;
+  virtual void deleteGroup(std::string nodes)=0;
+  virtual void deleteEmployee(std::string fname, std::string lname, std::string pos)=0;
+};
+#endif	/* BUILDER_H */
